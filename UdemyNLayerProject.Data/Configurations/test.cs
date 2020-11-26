9f3ee3a29a5cb85379639816c2c455337f4a7433
Erdem <erdemcalik1@gmail.com>
2020-11-26 21:27:55 +0300

    Yeni

---
+++ UdemyNLayerProject.Data/Configurations/test.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UdemyNLayerProject.Data.Configurations
+{
+    public class test
+    {
+        public int  test { get; set; }
+    }
+}
