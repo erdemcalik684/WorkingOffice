e18846c157b1f297072be97d4a0c4c3e89f3f077
erdemcalik684 <62620690+erdemcalik684@users.noreply.github.com>
2020-11-28 01:22:11 +0300

    test 2 güncelleme
    test

---
+++ UdemyNLayerProject.Core/UnitOfWorks/test2.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UdemyNLayerProject.Core.UnitOfWorks
+{
+    public class test2
+    {
+        public int Id { get; set; }
+    }
+}
